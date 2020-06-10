311f805e59cab5ec38596713139787dbba464b31
James <erresen@users.noreply.github.com>
2020-06-10 13:47:33 +0100

    remove > from list titles. move > to css in posts

--- js/overtype.js
+++ js/overtype.js
@@ -1,40 +1,39 @@
 function overType(element) {
-    var text = element.text();
-    flashCaret(element, text, 2, 0);
+  var text = element.text();
+  flashCaret(element, text, 2, 0);
 }
 
 function flashCaret(element, text, count, current) {
-    element.html('> _<span class="white-out">' + text + '</span>')
-    setTimeout(function(){
-        element.html('> <span class="white-out">_' + text + '</span>')
-        current++;
-        if (current > count) {
-            typeNextLetter(element, text, 0);
-        } else {
-            setTimeout(flashCaret, 250, element, text, count, current);
-        }
-    }, 250);
-};
+  element.html('_<span class="white-out">' + text + "</span>");
+  setTimeout(function () {
+    element.html('<span class="white-out">_' + text + "</span>");
+    current++;
+    if (current > count) {
+      typeNextLetter(element, text, 0);
+    } else {
+      setTimeout(flashCaret, 250, element, text, count, current);
+    }
+  }, 250);
+}
 
 function typeNextLetter(element, text, nextLetterIndex) {
-    if (nextLetterIndex < text.length) {
+  if (nextLetterIndex < text.length) {
+    var pretext = text.substring(0, nextLetterIndex);
+    var posttext = text.substring(nextLetterIndex);
+    var html = pretext + '<span class="white-out">' + posttext + "</span>";
 
-        var pretext = text.substring(0, nextLetterIndex);
-        var posttext = text.substring(nextLetterIndex);
-        var html = '> ' + pretext + '<span class="white-out">' + posttext + '</span>';
-        
-        element.html(html);
+    element.html(html);
 
-        var nextLetter = text[nextLetterIndex];
-        var timeout = getRandomInt(1, 100);
-        if (nextLetter === ' '){
-            timeout += 100;
-        }
-        setTimeout(typeNextLetter, timeout, element, text, ++nextLetterIndex);
-    } else {
-        element.text('> ' + text);
-        console.log("Done!");
+    var nextLetter = text[nextLetterIndex];
+    var timeout = getRandomInt(1, 100);
+    if (nextLetter === " ") {
+      timeout += 100;
     }
+    setTimeout(typeNextLetter, timeout, element, text, ++nextLetterIndex);
+  } else {
+    element.text(text);
+    console.log("Done!");
+  }
 }
 
 function getRandomInt(min, max) {
